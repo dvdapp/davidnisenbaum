55e1b0a7cad1e301ebcf3709065e8ab45404fe88
David <dnisenbaum1@gmail.com>
2020-06-23 22:39:02 -0400

    change to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
+import { Component } from 'react';
 import Head from 'next/head';
-import { Link, Tooltip, Fade } from '@material-ui/core';
-import { makeStyles } from "@material-ui/core/styles";
+import { Link, Tooltip, Fade, CircularProgress } from '@material-ui/core';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import YouTubeIcon from '@material-ui/icons/YouTube';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme: Theme) => createStyles({
   root: {
     textAlign: "center",
     height: "100vh"
@@ -28,6 +29,10 @@ const useStyles = makeStyles((theme) => ({
         width: 45,
       }
     }
+  },
+
+  spinner: {
+    color: "#B0C4DE", 
   },
 
   github: {
@@ -72,7 +77,7 @@ const useStyles = makeStyles((theme) => ({
       color: "#ff6a00"
     }
   }
-}));
+});
 
 
 /*
@@ -103,53 +108,81 @@ instagram svg
 
 /*
 soundcloud svg
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" className={classes.soundcloud}><path d="M7 17.939h-1v-8.068c.308-.231.639-.429 1-.566v8.634zm3 0h1v-9.224c-.229.265-.443.548-.621.857l-.379-.184v8.551zm-2 0h1v-8.848c-.508-.079-.623-.05-1-.01v8.858zm-4 0h1v-7.02c-.312.458-.555.971-.692 1.535l-.308-.182v5.667zm-3-5.25c-.606.547-1 1.354-1 2.268 0 .914.394 1.721 1 2.268v-4.536zm18.879-.671c-.204-2.837-2.404-5.079-5.117-5.079-1.022 0-1.964.328-2.762.877v10.123h9.089c1.607 0 2.911-1.393 2.911-3.106 0-2.233-2.168-3.772-4.121-2.815zm-16.879-.027c-.302-.024-.526-.03-1 .122v5.689c.446.143.636.138 1 .138v-5.949z"/></svg>
-          
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" className={classes.soundcloud}><path d="M7 17.939h-1v-8.068c.308-.231.639-.429 1-.566v8.634zm3 0h1v-9.224c-.229.265-.443.548-.621.857l-.379-.184v8.551zm-2 0h1v-8.848c-.508-.079-.623-.05-1-.01v8.858zm-4 0h1v-7.02c-.312.458-.555.971-.692 1.535l-.308-.182v5.667zm-3-5.25c-.606.547-1 1.354-1 2.268 0 .914.394 1.721 1 2.268v-4.536zm18.879-.671c-.204-2.837-2.404-5.079-5.117-5.079-1.022 0-1.964.328-2.762.877v10.123h9.089c1.607 0 2.911-1.393 2.911-3.106 0-2.233-2.168-3.772-4.121-2.815zm-16.879-.027c-.302-.024-.526-.03-1 .122v5.689c.446.143.636.138 1 .138v-5.949z"/></svg>        
 */
 
-const Home = () => {
-  const classes = useStyles();
-  const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
-
-  return (
-    <div className={classes.root}>
-      <Head>
-        <title>David Nisenbaum</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossOrigin="anonymous"/>
-        <script src="https://kit.fontawesome.com/7a66ef7334.js" crossOrigin="anonymous"></script>
-      </Head>
-
-      <main className={classes.main}>
-        <Tooltip title="Check Out my Code" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
-          <Link href="https://github.com/dvdapp/">
-            <GitHubIcon classes={{ root: classes.github }} />
-          </Link>
-        </Tooltip>
-        <Tooltip title="View my Resume" placement="top" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
-          <Link href="https://www.linkedin.com/in/davidnisenbaum/">
-            <LinkedInIcon classes={{ root: classes.linkedin }} />
-          </Link>
-        </Tooltip>
-        <Tooltip title="Watch my Adventures" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
-          <Link href="https://www.youtube.com/channel/UCYRrV-BJKHndebk1F09d1FQ/">
-            <YouTubeIcon classes={{ root: classes.youtube }}/>
-          </Link>
-        </Tooltip> 
-        <Tooltip title="Follow my Social Life" placement="top" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
-          <Link href="https://www.instagram.com/david.nisenn/">
-            <i className={`${classes.instagram} fab fa-instagram`} />
-          </Link>
-        </Tooltip>
-        <Tooltip title="Listen to my Music" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
-          <Link href="https://soundcloud.com/neighb0urs/">
-            <i className={`${classes.soundcloud} fab fa-soundcloud`} />
-          </Link>
-        </Tooltip>
-      </main>
-    </div>
-  );
+interface Props {
+  classes: any;
 }
 
+interface State {
+  mounted: boolean;
+}
 
-export default Home
+class Home extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mounted: false,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ mounted: true});
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Head>
+          <title>David Nisenbaum</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossOrigin="anonymous"/>
+          <script src="https://kit.fontawesome.com/7a66ef7334.js" crossOrigin="anonymous"></script>
+        </Head>
+
+        <main className={classes.main}>
+          {this.state.mounted && 
+          (
+            <>
+              <Tooltip title="Check Out my Code" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
+                <Link href="https://github.com/dvdapp/">
+                  <GitHubIcon classes={{ root: classes.github }} />
+                </Link>
+              </Tooltip>
+              <Tooltip title="View my Resume" placement="top" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
+                <Link href="https://www.linkedin.com/in/davidnisenbaum/">
+                  <LinkedInIcon classes={{ root: classes.linkedin }} />
+                </Link>
+              </Tooltip>
+              <Tooltip title="Watch my Adventures" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
+                <Link href="https://www.youtube.com/channel/UCYRrV-BJKHndebk1F09d1FQ/">
+                  <YouTubeIcon classes={{ root: classes.youtube }}/>
+                </Link>
+              </Tooltip> 
+              <Tooltip title="Follow my Social Life" placement="top" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
+                <Link href="https://www.instagram.com/david.nisenn/">
+                  <i className={`${classes.instagram} fab fa-instagram`} />
+                </Link>
+              </Tooltip>
+              <Tooltip title="Listen to my Music" arrow TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}>
+                <Link href="https://soundcloud.com/neighb0urs/">
+                  <i className={`${classes.soundcloud} fab fa-soundcloud`} />
+                </Link>
+              </Tooltip>
+            </>
+          )
+          }
+
+          {!this.state.mounted && (
+            <CircularProgress className={classes.spinner} />
+          )}
+        </main>
+      </div>
+    );
+  }
+}
+
+export default withStyles(useStyles)(Home)
